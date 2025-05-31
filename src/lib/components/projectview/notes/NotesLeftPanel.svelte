50487acaea08fa111263d543eba7ef4742e8bde3
Dipanjan Saha <dipanjansaha@hotmail.com>
2025-05-31 08:21:34 +0100

    minor fix

--- src/lib/components/projectview/notes/NotesLeftPanel.svelte
+++ src/lib/components/projectview/notes/NotesLeftPanel.svelte
@@ -435,8 +435,6 @@
       return () => document.removeEventListener('click', listener);
     });
 
-    $: selectedItemPathInStore = $project.selectedDocumentPath || $project.currentImportedTranscriptPath || $project.selectedMediaNotePath;
-
     // Metadata related functions (loadMetadata, toggleEditMode, handleSaveMetadata) and reactive blocks removed.
     // selectedItemPathInStore is kept for now, assuming it might be used for other UI elements like selection highlighting.
     $: selectedItemPathInStore = $project.selectedDocumentPath || $project.currentImportedTranscriptPath || $project.selectedMediaNotePath;
