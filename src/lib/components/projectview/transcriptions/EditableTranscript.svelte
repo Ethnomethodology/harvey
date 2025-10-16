5b40ba392a469044b8b13b57bb622abf63a92b5f
Dipanjan Saha <dipanjansaha@hotmail.com>
2025-10-16 13:14:48 +0100

    removed unnecessary buttons

--- src/lib/components/projectview/transcriptions/EditableTranscript.svelte
+++ src/lib/components/projectview/transcriptions/EditableTranscript.svelte
@@ -583,14 +583,7 @@ import { ExtendedTextNode } from '$lib/nodes/ExtendedTextNode.js';
                 <div class="flex justify-center">
                     <div class="{columnContainerClass}" style="width: 40.01rem; font-family: Arial, Helvetica, sans-serif; font-size: 12pt; line-height: 1.5;">
 
-                        <!-- Add Segment Before Button -->
-                        {#if editEnabled}
-                        <div class="flex justify-center my-2">
-                            <button on:click={() => dispatch('insertnewsegment', { index: currentIndex, before: true })} class="btn-icon text-green-500 hover:text-green-700" title="Insert New Segment Before" aria-label="Insert New Segment Before">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16"> <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0"/> </svg>
-                            </button>
-                        </div>
-                        {/if}
+
 
                         <!-- Primary Segment Editor -->
                         <div class="primary-segment-editor">
@@ -806,14 +799,7 @@ import { ExtendedTextNode } from '$lib/nodes/ExtendedTextNode.js';
                             </div>
                         {/if}
 
-                        <!-- Add Segment After Button -->
-                        {#if editEnabled}
-                        <div class="flex justify-center my-2">
-                            <button on:click={() => dispatch('insertnewsegment', { index: currentIndex, before: false })} class="btn-icon text-green-500 hover:text-green-700" title="Insert New Segment After" aria-label="Insert New Segment After">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16"> <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0"/> </svg>
-                            </button>
-                        </div>
-                        {/if}
+
                     </div>
                 </div>
             </div>
