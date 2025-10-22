f17a9c5cbd5fdee03f646035c9030020f9fa6b88
Dipanjan Saha <dipanjansaha@hotmail.com>
2025-10-22 17:09:17 +0100

    removed unused script

--- src-tauri/src/welcome/python_env.rs
+++ src-tauri/src/welcome/python_env.rs
@@ -243,7 +243,7 @@ async fn install_python_libraries_micromamba<R: Runtime>(app: &AppHandle<R>, she
 
     let strategy = get_pytorch_install_strategy(shell).await;
     let mut pip_packages = vec![
-        "torch==2.9.0", "torchvision==0.24.0", "torchaudio==2.9.0",
+        "torch==2.9.0", "torchaudio==2.9.0",
         "pyannote.audio==4.0.1", "pypandoc==1.15",
         "transformers==4.57.1", "sacremoses==0.1.1", "sentencepiece==0.2.1", "torchcodec==0.8.0"
     ];
