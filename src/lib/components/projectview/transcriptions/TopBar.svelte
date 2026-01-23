1e2cb90efbf933538eac2159201771cb6b2ab450
Dipanjan Saha <dipanjansaha@hotmail.com>
2026-01-23 10:31:42 +0000

    Fix import error for insertTranscriptSegment in TopBar

--- src/lib/components/projectview/transcriptions/TopBar.svelte
+++ src/lib/components/projectview/transcriptions/TopBar.svelte
@@ -8,7 +8,7 @@
 	import { themePreference, cycleThemePreference } from '$lib/stores/themeStore.js';
 
 	// --- Service Imports ---
-	import { requestTranscription, requestTranslation, insertTranscriptSegment } from '$lib/services/projectService.js';
+	import { requestTranscription, requestTranslation } from '$lib/services/projectService.js';
 	import { getDownloadedModels, exportTranscript } from '$lib/services/configureActions.js';
 
 	// --- Tauri Imports ---
