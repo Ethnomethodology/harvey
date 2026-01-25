2e8a9ed62023c2bdd99f16c08c7faedae6a93c9d
Dipanjan Saha <dipanjansaha@hotmail.com>
2026-01-25 23:33:27 +0000

    feat(ui): add text labels and rearrange buttons in Data Top Bar

--- src/lib/components/projectview/data/DataTopBar.svelte
+++ src/lib/components/projectview/data/DataTopBar.svelte
@@ -533,10 +533,11 @@
   >
     <div class="flex items-center space-x-1.5 min-w-0"> <!-- Added min-w-0 for truncate to work -->
         <div class="h-10 flex items-center justify-center flex-shrink-0">
-            <button title="Import" aria-label="Import" class="ui-button-import hover-scale-effect ml-1 mr-1" on:click={(e) => dispatch('requestImport', e)}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <button title="Import" aria-label="Import" class="ui-button-icon flex items-center ml-1 mr-1 hover-scale-effect" on:click={(e) => dispatch('requestImport', e)}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                 </svg>
+                <span class="text-xs ml-1">Import</span>
             </button>
         </div>
         
@@ -544,6 +545,7 @@
         {#if $activeMediaFile}
         <button class="ui-button-icon flex items-center ml-2 space-x-0.5 hover-scale-effect"
             on:click={() => dispatch('requestTranscriptionTabWithMediaAndDialog', { mediaPath: $activeMediaFile.path })}
+            title="Transcribe"
         >
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
@@ -552,7 +554,7 @@
         </button>
         {/if}
         {#if $project.activeDocumentEditorRef}
-        <button class="ui-button-icon flex items-center ml-2 hover-scale-effect" on:click={toggleLiveTranscription} title="Live Transcription">
+        <button class="ui-button-icon flex items-center ml-2 space-x-0.5 hover-scale-effect" on:click={toggleLiveTranscription} title="Live Transcribe">
             {#if isLiveTranscriptionActive}
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mic-fill" class:blinking-red-text={isLiveTranscriptionActive} viewBox="0 0 16 16">
                 <path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0z"/>
@@ -564,6 +566,7 @@
                 <path d="M10 8a2 2 0 1 1-4 0V3a2 2 0 1 1 4 0zM8 0a3 3 0 0 0-3 3v5a3 3 0 0 0 6 0V3a3 3 0 0 0-3-3"/>
             </svg>
             {/if}
+            <span class="text-xs">Live Transcribe</span>
         </button>
         {/if}
     </div>
@@ -581,6 +584,14 @@
             <button class="ui-button-icon flex items-center space-x-0.5 hover-scale-effect" on:click="{() => isExportModalOpen = true}" title="Export Transcript" >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4"> <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" /> </svg>
                 <span class="text-xs">Export</span>
+            </button>
+        {/if}
+        {#if isLexicalDocument}
+            <button class="ui-button-icon flex items-center space-x-0.5 hover-scale-effect" on:click="{() => showTranslateDocumentModal = true}" title="Translate Document" >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 01-3.827-5.802" />
+                </svg>
+                <span class="text-xs">Translate</span>
             </button>
         {/if}
         {#if isImportedTranscript}
@@ -601,14 +612,6 @@
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-layout-split" viewBox="0 0 16 16" style="transform: rotate(90deg);">
                     <path d="M0 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm8.5-1v12H14a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1zm-1 0H2a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h5.5z"/>
                 </svg>
-            </button>
-        {/if}
-        {#if isLexicalDocument}
-            <button class="ui-button-icon flex items-center space-x-0.5 hover-scale-effect" on:click="{() => showTranslateDocumentModal = true}" title="Translate Document" >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 01-3.827-5.802" />
-                </svg>
-                <span class="text-xs">Translate</span>
             </button>
         {/if}
         <button
