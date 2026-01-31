160fc94c26b05a15d3778e7542a31df1572fad4d
Dipanjan Saha <dipanjansaha@hotmail.com>
2026-02-01 01:19:22 +0530

    Refine CTranslate2 installation info UI
    - Removed recommendation/speed-boost text for a cleaner look.
    - Aligned CTranslate2 installation info icon and text.

--- src/lib/components/shared/TranslationConfiguration.svelte
+++ src/lib/components/shared/TranslationConfiguration.svelte
@@ -440,15 +440,11 @@
 
 			{#if !ct2Installed}
 				<div class="bg-orange-50 dark:bg-orange-900/20 border border-orange-200 dark:border-orange-800 rounded p-2 text-[11px] text-orange-800 dark:text-orange-300">
-					<div class="flex items-start">
-						<svg class="w-3.5 h-3.5 mr-1.5 mt-0.5 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
+					<div class="flex items-center">
+						<svg class="w-3.5 h-3.5 mr-1.5 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
 							<path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
 						</svg>
-						<div>
-							<span class="font-bold uppercase tracking-wider text-[9px]">Recommendation:</span>
-							<p class="mt-0.5">Helsinki models can run <strong class="text-orange-900 dark:text-orange-200">2-4x faster</strong> on your CPU using <strong class="text-orange-900 dark:text-orange-200">CTranslate2</strong>.</p>
-							<p class="mt-1 opacity-80">It will be installed automatically along with your first model download.</p>
-						</div>
+						<p>CTranslate2 will be installed automatically along with your first model download.</p>
 					</div>
 				</div>
 			{/if}
