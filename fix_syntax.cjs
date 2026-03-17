a1e4902fe0404f989a9aa308bbc5084e29a807c4
Dipanjan Saha <dipanjansaha@hotmail.com>
2026-03-17 12:35:33 +0000

    chore: remove temporary patch and test scripts from root

--- fix_syntax.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const file = 'src/lib/components/projectview/data/tables/ChartModal.svelte';
-let content = fs.readFileSync(file, 'utf8');
-
-// Replace double declarations
-content = content.replace(/let prevOpen = false;\n\s*let prevOpen = false;/g, 'let prevOpen = false;');
-
-fs.writeFileSync(file, content);
