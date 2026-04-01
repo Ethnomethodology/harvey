0088caf76ba3d10c66c9187dbd0f028b31590e47
Dipanjan Saha <dipanjansaha@hotmail.com>
2026-04-01 21:10:59 +0100

    fix(DataTopBar): match import button icon size to Transcription tab (w-5 h-5)

--- src/lib/components/projectview/data/DataTopBar.svelte
+++ src/lib/components/projectview/data/DataTopBar.svelte
@@ -570,7 +570,7 @@
             title="Import Audio or Video"
             aria-label="Import Audio or Video"
         >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-4 h-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
             </svg>
         </button>
